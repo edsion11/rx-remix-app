1d6d19a91060bc1700357b2a31bfca2a525887ff
guzhenxu <guzhenxu0@gmail.com>
2021-12-20 19:54:58 +0800

    Update remix.config.js

--- remix.config.js
+++ remix.config.js
@@ -5,6 +5,5 @@ module.exports = {
   appDirectory: "app",
   browserBuildDirectory: "public/build",
   publicPath: "/build/",
-  serverBuildDirectory: "api/build",
-  devServerPort: 8002
+  serverBuildDirectory: "api/_build"
 };
