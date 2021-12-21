2af12e0b316c3fa1694c0b630562017babc91dd1
guzhenxu <guzhenxu0@gmail.com>
2021-12-21 23:58:15 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,8 +1,8 @@
 const { createRequestHandler } = require("@remix-run/vercel");
 
-// module.exports = createRequestHandler({
-//   build: require("./_build")
-// });
-module.exports = function (req, res) {
-  res.send('Hello!');
-};
+module.exports = createRequestHandler({
+  build: require("./_build")
+});
+// module.exports = function (req, res) {
+//   res.send('Hello!');
+// };
